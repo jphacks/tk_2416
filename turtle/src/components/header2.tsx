3f1d14206a1962bdc0a9c70931803f3fa6036f72
Marie Muramatsu <muramatsumarie@gmail.com>
2024-10-27 02:02:21 +0900

    add header1 componenet

---
+++ turtle/src/components/header2.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+type Props = {
+    text: string; 
+}
+
+export const Header1: React.FC<Props> = ({text}) => {
+    return (
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+    
+        <Typography variant="h4" gutterBottom>
+          {text}
+        </Typography>
+       
+      </Box>
+    );
+  }
